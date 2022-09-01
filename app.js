0c36605dba87a8f7f407081833b2012867b70950
Fernando Orozco <forozcomercado@gmail.com>
2022-09-01 08:53:56 -0500

    UPDATE now we can get data through req.body

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const { testConnection } = require("./db.connection");
 
 app.use(morgan("dev"));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   return res.redirect(301, "/employees");
 });
