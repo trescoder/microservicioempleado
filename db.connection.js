1a53897a78d781ecea06dbd73fe09187c4b4c8cc
Fernando Orozco <forozcomercado@gmail.com>
2022-09-01 08:02:30 -0500

    ADD auto db sync to match  models

--- db.connection.js
+++ db.connection.js
@@ -9,10 +9,14 @@ const sequelize = new Sequelize(db.name, db.username, db.password, {
 async function testConnection() {
   try {
     await sequelize.authenticate();
+    await sequelize.sync({
+      force: true, // creates tables, drop them first if already exist
+      alter: true, // alter tables in the db to make them match the model
+    });
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 }
 
-module.exports = testConnection;
+module.exports = { sequelize, testConnection };
