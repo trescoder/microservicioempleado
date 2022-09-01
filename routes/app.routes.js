c8813653cee7fabe176a4909eb36f90b974bfefe
Fernando Orozco <forozcomercado@gmail.com>
2022-09-01 08:55:13 -0500

    ADD route to add new department ADD route to add new employee

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -3,4 +3,7 @@ const appController = require("../controllers/app.controller");
 
 router.get("/", appController.getAllEmployees);
 
+router.post("/add-department", appController.addDepartment);
+router.post("/add-employee", appController.addEmployee);
+
 module.exports = router;
